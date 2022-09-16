8c6b6c82c28ecd342426ca8ee99034e627f9bb9a
Kiran P <kiran.p@bazaarvoice.com>
2022-09-16 16:33:18 +0530

    added hypothesis field in ABFrom component, updated styling, labels and alignments

--- src/components/ABForm/index.jsx
+++ src/components/ABForm/index.jsx
@@ -6,7 +6,10 @@ import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Grid from '@mui/material/Grid';
 import dayjs from 'dayjs';
 import moment from "moment";
@@ -15,24 +18,18 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import "./style.css";
 
 export default function App() {
-  const onSubmit = (e) => {
-    e.preventDefault();
-    let finalData = {...formData};
-    finalData.duration = moment(formData.end) - moment(formData.start)
-
-    console.log('final Data : ', finalData);
-  }
 
   const [formData, setFormData] = useState({
-    start: "",
-    end: "",
-    client: "",
+    start: null,
+    end: null,
+    client: "testcustomer-fast-prs",
     deploymentZones: [],
     splits: [],
     implementation1: "",
-    implementation2: ""
+    implementation2: "",
+    hypothesis: ""
   });
-
+  
   const deploymentZoneItems = [
     {
       name: 'Deployment 1',
@@ -66,108 +63,142 @@ export default function App() {
     },
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let finalData = {...formData};
+    finalData.start = moment(formData.start);
+    finalData.end = moment(formData.end);
+    finalData.duration = formData.end.diff(formData.start)
+
+    console.log('final Data : ', finalData);
+  }
   const onChange = (fieldValue, fieldKey) => {
-    console.log('e: ', fieldValue);
     setFormData({...formData, [fieldKey]: fieldValue})
   }
 
   return (
     <Container className="abFormContainer" fluid="true">
-        <Box style={{padding: "20px"}}  sx={{ bgcolor: '#FAFAFA',height: '100vh', flexGrow: 1  }}>
+        <Box style={{padding: "20px"}}  sx={{ bgcolor: '#FAFAFA', flexGrow: 1  }}>
           <form onSubmit={onSubmit}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
             <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <InputLabel id="start-label">Start</InputLabel>
-                <DateTimePicker
-                  labelId="start-label"
-                  value={formData.start}
-                  onChange={(e) => onChange(moment(e).format(), 'start')}
-                  renderInput={(params) => <TextField {...params} />}
-                  className="inputField"
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <InputLabel id="end-label">End</InputLabel>
-                <DateTimePicker
-                  labelId="end-label"
-                  value={formData.end}
-                  minDateTime={dayjs(formData.start)}
-                  onChange={(e) => onChange(moment(e).format(), 'end')}
-                  renderInput={(params) => <TextField {...params} />}
-                  className="inputField"
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <InputLabel id="client-label">End</InputLabel>
-                <TextField 
-                  className="inputField" 
-                  labelId="client-label"
-                  variant="outlined" 
-                  onChange={(e) => onChange(e.target.value, 'client')} 
-                  value={formData.client}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <InputLabel id="deployment-zone-label">Deployment Zones</InputLabel>
-                <Select
-                  labelId="deployment-zone-label"
-                  value={formData.deploymentZones}
-                  onChange={(e) => onChange(e.target.value, 'deploymentZones')}
-                  multiple
-                  MenuProps={MenuProps}
-                  className="inputField"
-                >
-                  {deploymentZoneItems.map((item) => (
-                    <MenuItem
-                      key={item.name}
-                      value={item.value}
-                    >
-                      {item.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </Grid>
-              <Grid item xs={6}>
-                <InputLabel id="implementation1-label">Implementation 1</InputLabel>
-                <Select
-                  labelId="implementation1-label"
-                  value={formData.implementation1}
-                  onChange={(e) => onChange(e.target.value, 'implementation1')}
-                  MenuProps={MenuProps}
-                  className="inputField"
-                >
-                  {implementationItems.map((item) => (
-                    <MenuItem
-                      key={item.name}
-                      value={item.value}
-                    >
-                      {item.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </Grid>
-              <Grid item xs={6}>
-                <InputLabel id="implementation2-label">Implementation 2</InputLabel>
-                <Select
-
-                  value={formData.implementation2}
-                  onChange={(e) => onChange(e.target.value, 'implementation2')}
-                  MenuProps={MenuProps}
-                  labelId="implementation2-label"
-                  className="inputField"
-                >
-                  {implementationItems.map((item) => (
-                    <MenuItem
-                      key={item.name}
-                      value={item.value}
-                    >
-                      {item.name}
-                    </MenuItem>
-                  ))}
-                </Select> 
-              </Grid>
               <Grid item xs={12}>
+                <div className="fieldContainer" style={{width: '100%'}}>
+                  <InputLabel id="hypothesis-label">Hypothesis</InputLabel>
+                  <TextareaAutosize
+                    labelid="hypothesis-label"
+                    minRows={3}
+                    style={{ width: '100%' }}
+                    value={formData.hypothesis}
+                    onChange={(e) => onChange(e, 'hypothesis')}
+                  />
+                </div>
+              </Grid>
+              <Grid item xs={6}>
+                <div className="fieldContainer">
+                  <InputLabel id="start-label">Start Date</InputLabel>
+                  <DateTimePicker
+                    labelid="start-label"
+                    value={formData.start}
+                    onChange={(e) => onChange(e, 'start')}
+                    renderInput={(params) => <TextField {...params} />}
+                    className="inputField"
+                  />
+                </div>
+              </Grid>
+              <Grid item xs={6}>
+                <div className="fieldContainer">
+                  <InputLabel id="end-label">End Date</InputLabel>
+                  <DateTimePicker
+                    labelid="end-label"
+                    value={formData.end}
+                    minDateTime={dayjs(formData.start)}
+                    onChange={(e) => onChange(e, 'end')}
+                    renderInput={(params) => <TextField {...params} />}
+                    className="inputField"
+                  />
+                </div>
+              </Grid>
+              <Grid item xs={6}>
+                <div className="fieldContainer">
+                  <InputLabel id="client-label">Client</InputLabel>
+                  <TextField 
+                    disabled
+                    className="inputField" 
+                    labelid="client-label"
+                    variant="outlined" 
+                    onChange={(e) => onChange(e.target.value, 'client')} 
+                    value={formData.client}
+                  />
+                </div>
+              </Grid>
+              <Grid item xs={6}>
+                <div className="fieldContainer">
+                  <InputLabel id="deployment-zone-label">Deployment Zones</InputLabel>
+                  <Select
+                    labelid="deployment-zone-label"
+                    value={formData.deploymentZones}
+                    onChange={(e) => onChange(e.target.value, 'deploymentZones')}
+                    multiple
+                    renderValue={(selected) => selected.join(', ')}
+                    MenuProps={MenuProps}
+                    className="inputField"
+                  >
+                    {deploymentZoneItems.map((item) => (
+                      <MenuItem
+                        key={item.name}
+                        value={item.value}
+                      >
+                        <Checkbox checked={formData.deploymentZones.indexOf(item.value) > -1} />
+                        <ListItemText primary={item.name} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </div>
+              </Grid>
+              <Grid item xs={6}>
+                <div className="fieldContainer">
+                  <InputLabel id="implementation1-label">Template 1</InputLabel>
+                  <Select
+                    labelid="implementation1-label"
+                    value={formData.implementation1}
+                    onChange={(e) => onChange(e.target.value, 'implementation1')}
+                    MenuProps={MenuProps}
+                    className="inputField"
+                  >
+                    {implementationItems.map((item) => (
+                      <MenuItem
+                        key={item.name}
+                        value={item.value}
+                      >
+                        {item.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </div>
+              </Grid>
+              <Grid item xs={6}>
+                <div className="fieldContainer">
+                  <InputLabel id="implementation2-label">Template 2</InputLabel>
+                  <Select
+                    value={formData.implementation2}
+                    onChange={(e) => onChange(e.target.value, 'implementation2')}
+                    MenuProps={MenuProps}
+                    labelid="implementation2-label"
+                    className="inputField"
+                  >
+                    {implementationItems.map((item) => (
+                      <MenuItem
+                        key={item.name}
+                        value={item.value}
+                      >
+                        {item.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </div>
+              </Grid>
+              <Grid item xs={12} style={{}} sx={{mt:4}}>
                 <Button size="large" variant="contained" type="submit">SUBMIT</Button>
               </Grid>
             </Grid>
